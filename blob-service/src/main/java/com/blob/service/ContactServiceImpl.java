59940e6e0e03acd9673c6eec8aabef5039720599
rajupbiz <rajupatel.biz@gmail.com>
2016-01-01 15:37:30 +0530

    init

---
+++ blob-service/src/main/java/com/blob/service/ContactServiceImpl.java
@@ -0,0 +1,20 @@
+package com.blob.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.blob.dao.ContactDao;
+
+@Service
+public class ContactServiceImpl implements ContactService {
+
+	@Resource
+	ContactDao contactDao;
+	
+	@Override
+	public Long getContactCount() {
+		return contactDao.count();
+	}
+
+}
---
+++ blob-service/src/test/java/com/blob/BlobServiceApplicationTests.java
@@ -0,0 +1,35 @@
+package com.blob;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.blob.service.ContactServiceImpl;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BlobServiceApplication.class)
+public class BlobServiceApplicationTests {
+
+	@Resource
+	private ContactServiceImpl contactService;
+	
+	@Test
+	public void contextLoads() {
+		
+	}
+
+	@Test
+	public void testCore(){
+		
+		Long noOfContacts = contactService.getContactCount();
+		
+		System.out.println(" noOfContacts >>  "+noOfContacts);
+		
+		
+		System.out.println(" noOfContacts >>  "+noOfContacts);
+	}
+	
+}
