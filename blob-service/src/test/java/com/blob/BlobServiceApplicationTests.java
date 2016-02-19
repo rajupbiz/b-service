cb9edf9c152fa7d74916bdb2f62aa1246c3a31d5
rajupbiz <rajupatel.biz@gmail.com>
2016-02-19 13:31:06 +0530

    ii

--- blob-service/src/test/java/com/blob/BlobServiceApplicationTests.java
+++ blob-service/src/test/java/com/blob/BlobServiceApplicationTests.java
@@ -1,20 +1,16 @@
 package com.blob;
 
-import javax.annotation.Resource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.blob.service.impl.ContactServiceImpl;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = BlobServiceApplication.class)
 public class BlobServiceApplicationTests {
 
-	@Resource
-	private ContactServiceImpl contactService;
+	/*@Resource
+	private ContactServiceImpl contactService;*/
 	
 	@Test
 	public void contextLoads() {
@@ -24,12 +20,12 @@ public class BlobServiceApplicationTests {
 	@Test
 	public void testCore(){
 		
-		Long noOfContacts = contactService.getContactCount();
+		/*Long noOfContacts = contactService.getContactCount();
 		
 		System.out.println(" noOfContacts >>  "+noOfContacts);
 		
 		
-		System.out.println(" noOfContacts >>  "+noOfContacts);
+		System.out.println(" noOfContacts >>  "+noOfContacts);*/
 	}
 	
 }
